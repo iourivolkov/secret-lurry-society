384c3464e8256e07fcc36fc72822b6e76919c8ee
Iouri Volkov <volk.iouri@gmail.com>
2022-08-10 00:24:25 -0400

    Add lurry asset, create Landing Page component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navigation from "./components/Navigation/Navigation";
+import LandingPage from "./components/LandingPage/LandingPage";
 import "./app.css";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
+      <LandingPage />
     </div>
   );
 }
---
+++ src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,12 @@
+import originalLurry from "../../assets/originalLurry.PNG";
+import "./landingPage.css";
+
+const LandingPage = () => {
+  return (
+    <div className="landing-page-assets">
+      <img src={originalLurry} className="lurry-image" />
+    </div>
+  );
+};
+
+export default LandingPage;
