d090b1294b2d87c134747ec26d7c5cc2fd3486f9
Iouri Volkov <volk.iouri@gmail.com>
2022-08-09 23:16:53 -0400

    Create main page structure, add css styles for Navigation component

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,15 @@
+import "./navigation.css";
+
+const Navigation = () => {
+  return (
+    <>
+      <ul className="nav-links">
+        <li className="button">Roadmap</li>
+        <li className="button">Merch</li>
+        <li className="button">Lurry Labs</li>
+      </ul>
+    </>
+  );
+};
+
+export default Navigation;
